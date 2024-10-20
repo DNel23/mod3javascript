9a97520a065136e19e30e0a1a9ed6b162a34811d
Nelson Donoso <nelsondonoso@gmail.com>
2024-10-20 17:55:11 -0300

    Modulo 3 Desafio 01 - JavaScript

---
+++ index.js
@@ -0,0 +1,9 @@
+precio = 400000
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio
+
+valor_total = 0
+valor_totalSpan = document.querySelector(".valor-total");
+valor_totalSpan.innerHTML = valor_total
+
+
